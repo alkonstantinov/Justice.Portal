837c121d1c63fc93fc5a52db8f56bfec4cd0f6c7
alkonstantinov <alkonstantinov@outlook.com>
2019-08-02 19:54:57 +0300

    .

--- Justice.Portal.WebAPI/Justice.Portal.Web/wwwroot/js/mjprocess.js
+++ Justice.Portal.WebAPI/Justice.Portal.Web/wwwroot/js/mjprocess.js
@@ -2,11 +2,15 @@
 class MJProcess {
     translation = {};
     language = "bg";
+    MJPageData = {};
 
     constructor() {
         this.LoadTranslations = this.LoadTranslations.bind(this);
         this.Translate = this.Translate.bind(this);
         this.DoProcess = this.DoProcess.bind(this);
+        this.PutBlocks = this.PutBlocks.bind(this);
+        this.PutElement = this.PutElement.bind(this);
+        this.PutLive = this.PutLive.bind(this);
         this.LoadTranslations();
     }
 
@@ -49,14 +53,69 @@ class MJProcess {
 
     }
 
+    PutElement(e) {
+        let blockId = $(e).attr("id");
+        let blockTypeId = $(e).attr("mjblocktypeid");
+        switch (blockTypeId) {
+            case "live": this.PutLive(blockId); break;
+        }
+    }
+
+    PutLive(divId) {
+        var oldDiv = $("#" + divId);
+        var obj = this.MJPageData["block_" + divId];
+        var self = this;
+        oldDiv.replaceWith($(`
+            <div class= "port-wrapper grid-item-emission" >
+            <div class="port-head">
+                <h3 class="port-title"><t>emissions</t></h3>
+                <div class="port-link-item">
+                </div>
+            </div>
+            <div class="port-box p-0 bgr-black box-border height-350">
+                <div class="abs-content" style='background-image: url("api/part/GetBlob?hash=`+ obj.blockData.imageId + `");'>
+                    <div class="abs-cover"></div>
+                    <div class="emission-label">
+                        <img src="images/live-symbol.png">
+                            <span><t>live</t></span>
+						</div>
+                        <div class="emission-title">
+                            <h2 class="white">`+ obj.blockData.title[self.language] + `                                
+							</h2>
+                            <a role="button" class="btn btn-emission js-video" data-toggle="modal" data-src="`+ obj.blockData.url + `" data-target="#liveEmission">
+                                <svg class="icon icon-play-button"><use xlink: href="images/symbol-defs.svg#icon-play-button"></use></svg>
+                            <t>watchlive</t>
+							</a>
+                    </div>
+                </div>
+            </div>
+			</div>
+             `));
 
 
-    DoProcess() {
+    }
+
+    PutBlocks() {
+        let array = $("div[mjblocktypeid]").each(
+            (i, e) => {
+
+                this.PutElement(e);
+
+            });
+    }
+
+    DoProcess(MJPageData) {
+        this.MJPageData = MJPageData;
+        this.PutBlocks();
         var self = this;
         $("T").each(function (i, e) {
             self.Translate(e);
         });
+
     }
+
+
+
 
     SwitchLanguage() {
         localStorage.setItem("language", this.language === "bg" ? "en" : "bg");
@@ -65,6 +124,3 @@ class MJProcess {
 
 }
 
-var mjProcess = new MJProcess();
-
-mjProcess.DoProcess();
--- justice.portal.admin/src/components/blocks/blocklive.js
+++ justice.portal.admin/src/components/blocks/blocklive.js
@@ -3,6 +3,7 @@ import BaseComponent from '../basecomponent';
 import { ToggleButton } from 'primereact/togglebutton';
 import TB from '../editors/tb';
 import eventClient from '../../modules/eventclient';
+import Comm from '../../modules/comm';
 
 export default class BlockLive extends BaseComponent {
 
@@ -19,16 +20,20 @@ export default class BlockLive extends BaseComponent {
         );
         this.Validate = this.Validate.bind(this);
         this.GetData = this.GetData.bind(this);
+        this.AddImage = this.AddImage.bind(this);
+
+
         var state = { lang: "bg" };
         if (this.props.block) {
             var obj = JSON.parse(this.props.block.jsonvalues);
             state.title = obj.title || {};
             state.url = obj.url;
+            state.imageId = obj.imageId;
         }
         else {
             state.title = {};
             state.url = "";
-            state.image = null;
+            state.imageId = null;
         }
 
 
@@ -45,10 +50,14 @@ export default class BlockLive extends BaseComponent {
     GetData() {
         return {
             title: this.state.title,
-            url: this.state.url
+            url: this.state.url,
+            imageId: this.state.imageId
         };
     }
 
+    AddImage(id) {
+        this.setState({ imageId: id });
+    }
 
     render() {
         var self = this;
@@ -73,6 +82,19 @@ export default class BlockLive extends BaseComponent {
                     <div className="col-12">
                         <label className="control-label">Линк към живото предаване</label>
                         <input type="url" className="form-control" value={self.state.url} onChange={(e) => self.setState({ url: e.target.value })}></input>
+                    </div>
+                </div>,
+                <div className="row">
+                    <div className="col-2">
+                        <button className="btn btn-success" onClick={() => self.UploadBlob(self.AddImage)}>Изображение</button>
+                    </div>
+                    <div className="col-2">
+                        <button className="btn btn-danger" onClick={() => self.setState({ imageId: null })}>Изчистване</button>
+                    </div>
+                    <div className="col-8">
+                        <img src={self.state.imageId ? Comm.url + "part/GetBlob?hash=" + self.state.imageId : null} alt="" style={{ 'max-width': '300px' }}>
+
+                        </img>
                     </div>
                 </div>
             ]
